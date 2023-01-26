74f4920d037896eca757b00d8f3833533107aa66
bah8454 <bah8454@rit.edu>
2023-01-26 16:33:06 -0500

    Stubbed tokenize function.

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        ArrayList<Token> tokenList = tokenize(args[0]);
+    }
+
+    public static ArrayList<Token> tokenize(String filename) {
+
+    }
+}
