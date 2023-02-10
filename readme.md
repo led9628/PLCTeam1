7f7eadbc5af55321e0258ee067597d5831923677
bah8454 <bah8454@rit.edu>
2023-02-10 18:07:39 -0500

    Updated readme information regarding how to run the files

--- readme.md
+++ readme.md
@@ -9,4 +9,4 @@
 
 # How To Run
 ## Phase 1:
-Simply compile and run the JottTokenizerTester.java file.
+Compile Main.java with `javac Main.java` and then run the file with command line arguments specifying the file to be read.  For example, `java Main tokenizerTestCases/strings.jott output.jott Jott` would run the tokenization step on `strings.jott` and ignore the next two arguments, outputting a list of found tokens.
--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,14 @@ import provided.JottTokenizer;
 class Main {
     public static void main(String[] args) {
         if (args.length != 3) {
-            // TODO: Yell at the user for not having the right command line arguments.
+            System.out.println("Invalid number of arguments.");
         } else {
             ArrayList<Token> tokenList = JottTokenizer.tokenize(args[0]);
+            if (tokenList != null) {
+                for (Token token : tokenList) {
+                    System.out.println(token.getTokenType() + ": " + token.getToken());
+                }
+            }
         }
         
         
