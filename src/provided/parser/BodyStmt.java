8a87f63b316867a5cf657523f35d88155431ebb0
bah8454 <bah8454@rit.edu>
2023-03-09 16:40:37 -0500

    w

--- src/provided/parser/BodyStmt.java
+++ src/provided/parser/BodyStmt.java
@@ -10,7 +10,7 @@ public class BodyStmt implements JottTree {
     public BodyStmt(ArrayList<Token> tokens)  throws ConstructionFailure{ // <if_stmt> | <while_loop> | <stmt>
         if (tokens.get(0).getToken().equals("if"))
             children.add(new IfStmt(tokens));
-        else if (tokens.get(0).getToken().equals("While"))
+        else if (tokens.get(0).getToken().equals("while"))
             children.add(new WhileLoop(tokens));
         else
             children.add(new Stmt(tokens));
