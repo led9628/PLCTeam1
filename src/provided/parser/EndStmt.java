ec07065587fd97b73d05c00c51fbfe7759498c9b
Kyle Baptiste <kjb2070@rit.edu>
2023-03-09 10:06:24 -0500

    Added Body, BodyStmt, EndStmt, and ReturnStmt

---
+++ src/provided/parser/EndStmt.java
@@ -0,0 +1,41 @@
+package provided.parser;
+
+import provided.JottTree;
+
+
+public class EndStmt implements JottTree{
+
+   public EndStmt(){}
+
+   @Override
+   public String convertToJott() {
+       // TODO Auto-generated method stub
+       return null;
+   }
+
+   @Override
+   public String convertToJava(String className) {
+       // TODO Auto-generated method stub
+       return null;
+   }
+
+   @Override
+   public String convertToC() {
+       // TODO Auto-generated method stub
+       return null;
+   }
+
+   @Override
+   public String convertToPython() {
+       // TODO Auto-generated method stub
+       return null;
+   }
+
+   @Override
+   public boolean validateTree() {
+       // TODO Auto-generated method stub
+       return false;
+   }
+
+}
+
