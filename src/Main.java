98e561b67c934a948251f01595c7283794d0cd88
SohailK <sohail.kumar03@gmail.com>
2023-03-31 08:37:43 -0400

    Created config file and fixed Main

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,28 @@ import provided.JottTokenizer;
 import provided.JottTree;
 
 class Main {
+
+    /*
+     * Arguments for main:
+     * 0: Jott
+     * 1: input=> fileIn.jott
+     * 2: output=> fileOut.jott
+     * 3: output language(Jott, Java, C, Python)
+     */
     public static void main(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 4) {
             System.out.println("Invalid number of arguments.");
         } else {
-            ArrayList<Token> tokenList = JottTokenizer.tokenize(args[0]);
+            ArrayList<Token> tokenList = JottTokenizer.tokenize(args[1]);
             JottTree parseTree = JottParser.parse(tokenList);
-            String result = switch (args[2]) {
+            String result = switch (args[3]) {
                 case "Jott" -> parseTree.convertToJott();
-                case "Java" -> parseTree.convertToJava(args[2].substring(0, args[2].length()-5));
+                case "Java" -> parseTree.convertToJava(args[3].substring(0, args[3].length()-5));
                 case "Python" -> parseTree.convertToPython();
                 case "C" -> parseTree.convertToC();
                 default -> "Last command line argument is an invalid language.  Should be Jott, Java, Python, or C.";
             };
             System.out.println(result);
         }
-        
-        
     }
 }
