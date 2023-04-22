b7170ddf423a081cfb92cc5e8911db5db0067e0e
bah8454 <bah8454@rit.edu>
2023-04-21 22:14:54 -0400

    Okay, now Java works

--- src/out.py
+++ src/out.py
@@ -1,46 +1,5 @@
-def testPrintLoop (x ):
-    while ( x > 0 ) :
-        print ( x ) 
-        x = x - 1 
-            def testConcatLoop (y ):
-    x = len( y ) - 1 
-    output = y 
-    while ( x > 0 ) :
-        output = (output  +  y ) 
-        x = x - 1 
-            return output 
-def testIf (d ,x ):
-    if ( d > 5.1 ) :
-        print ( "Hi" ) 
-        elif ( d > 0.1 ) :
-        print ( x ) 
-        else :
-        print ( 3.2 * 4.0 ) 
-            def testIfLoop (x ):
-    output = 1 
-    _switch= True 
-    while ( x > 0 ) :
-        if ( _switch) :
-            output = output * 2 
-            _switch= False 
-            else :
-            _switch= True 
-                    x = x - 1 
-            return output 
-def foo ():
-    print ( "ran foo" ) 
-    def testFuncCallLoop (x ):
-    while ( x > 0 ) :
-        foo ( ) 
-        x = x - 2 
-            def main ():
-    testPrintLoop ( 5 ) 
-    _str= "a1" 
-    print ( testConcatLoop ( _str) ) 
-    testIf ( 1.1 , 3 ) 
-    intI = testIfLoop ( 3 ) 
-    print ( intI ) 
-    testFuncCallLoop ( 5 ) 
+def main ():
+    print ( "Hello World" ) 
     if (__name__ == "__main__"):
     main()
 
